09e5e85b5618a9a9fd053b21e1a29cafa1e8b2a5
yantoww <hardiantobangkit@gmail.com>
2025-05-15 18:03:52 +0700

    edit nilai

--- PencatatanNilaiMahasiswa/PencatatanNilaiMahasiswa/obj/Release/net8.0/PencatatanNilaiMahasiswa.AssemblyInfo.cs
+++ PencatatanNilaiMahasiswa/PencatatanNilaiMahasiswa/obj/Release/net8.0/PencatatanNilaiMahasiswa.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PencatatanNilaiMahasiswa")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2ed686e4243e779dd3e9fa01b98f2c14fdafbf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PencatatanNilaiMahasiswa")]
