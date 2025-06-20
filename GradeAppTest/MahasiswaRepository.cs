14ac5b2913d5b2b81f83912a0296294b02019e5a
Iamjustzero <faridfarhan444@gmail.com>
2025-06-20 13:23:50 +0700

    add unit test mahasiswa repository load data mahasiswa

---
+++ GradeAppTest/MahasiswaRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GradeApp; // pastikan namespace ini sesuai dengan namespace pada MahasiswaRepository dan Mahasiswa
+
+namespace GradeAppTest
+{
+    [TestClass]
+    public class MahasiswaRepositoryTests
+    {
+        [TestMethod]
+        public void LoadData_ShouldReturnListOfMahasiswa()
+        {
+            // Act
+            var result = MahasiswaRepository.LoadData();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(List<Mahasiswa>));
+        }
+    }
+}
